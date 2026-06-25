71d1d4567386b3de40e4859d34b444406dc4b631
artem.kozlov <artyom.kozloff@gmail.com>
2026-06-25 11:13:31 +0300

    Added SaveTools

---
+++ Promo_Calculator/Assets/Modules/Core/Editor/SaveTools.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Modules.Core.Editor
+{
+    public static class SaveTools
+    {
+        private const string FileName = "save.json";
+
+        [MenuItem("Tools/Clear Save")]
+        private static void ClearSave()
+        {
+            var savePath = Path.Combine(
+                Application.persistentDataPath,
+                FileName);
+
+            if (!File.Exists(savePath))
+            {
+                Debug.Log("Save file not found.");
+                return;
+            }
+
+            File.Delete(savePath);
+
+            Debug.Log($"Save deleted: {savePath}");
+        }
+    }
+}
+#endif
